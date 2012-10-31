84bbe49429b79ab2bfaf29f9cb46b4374ce5b5ed
Julien Thevenon <julien_thevenon@yahoo.fr>
2012-10-31 20:18:02 +0100

    Put dom tree type definition in a separated file

--- include/dom_analyzer_if.h
+++ include/dom_analyzer_if.h
@@ -2,14 +2,11 @@
 #define _DOM_ANALYZER_IF_H_
 
 #include "dom_analyzer_generic_if.h"
-#include "xmlParser.h"
-
 #include "diff_analyzer_if.h"
-
+#include "dom_tree.h"
 
 namespace osm_diff_analyzer_dom_if
 {
-  typedef XMLNode t_dom_tree;
   class dom_analyzer_if: public osm_diff_analyzer_if::diff_analyzer_if, public dom_analyzer_generic_if<XMLNode>
   {
     public:
---
+++ include/dom_tree.h
@@ -0,0 +1,10 @@
+#ifndef _DOM_TREE_H_
+#define _DOM_TREE_H_
+
+#include "xmlParser.h"
+namespace osm_diff_analyzer_dom_if
+{
+  typedef XMLNode t_dom_tree;
+}
+#endif // _DOM_TREE_H_
+//EOF
